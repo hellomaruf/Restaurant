9f77e004b453e4876106142456625d2ba1df6792
maruf-ahmed <marufahamed1110@gmail.com>
2024-09-20 23:43:36 +0600

    Added slider in feedback section

--- src/app/Components/Feedback.jsx
+++ src/app/Components/Feedback.jsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import feedbackImg from "../assets/feedbackImg.png";
 import profile from "../assets/profile.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay} from 'swiper/modules';
 
 function Feedback() {
   return (
@@ -9,27 +12,97 @@ function Feedback() {
       <div className="grid grid-cols-7 gap-6 justify-between items-center">
         <div className="col-span-4 space-y-4">
           <h1 className="text-4xl font-semibold">Customer Feedback</h1>
-          <p>
-            I recently dined at your restaurant and wanted to share my
-            experience. The food was absolutely delicious, and I was impressed
-            by the freshness of the ingredients. Each dish was bursting with
-            flavor, and the portion sizes were perfect. The service was quick
-            and efficient, and the staff was incredibly friendly and welcoming.
-          </p>
-          <div className="flex items-center gap-2">
-            <Image
-              src={profile}
-              alt="Description of image"
-              width={60}
-              height={200}
-              className="pb-2"
-            />
+          <Swiper 
+           autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className="mySwiper">
+            <SwiperSlide>
+              <div className="space-y-4">
+                <p>
+                  I recently dined at your restaurant and wanted to share my
+                  experience. The food was absolutely delicious, and I was
+                  impressed by the freshness of the ingredients. Each dish was
+                  bursting with flavor, and the portion sizes were perfect. The
+                  service was quick and efficient, and the staff was incredibly
+                  friendly and welcoming.
+                </p>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={profile}
+                    alt="Description of image"
+                    width={60}
+                    height={200}
+                    className="pb-2"
+                  />
 
-            <div className="">
-              <h5 className="text-[#AC1518] font-semibold">Tayyab Sohail</h5>
-              <h5>UX/UI Designer</h5>
-            </div>
-          </div>
+                  <div className="">
+                    <h5 className="text-[#AC1518] font-semibold">
+                      Tayyab Sohail
+                    </h5>
+                    <h5>UX/UI Designer</h5>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="space-y-4">
+                <p>
+                  I recently dined at your restaurant and wanted to share my
+                  experience. The food was absolutely delicious, and I was
+                  impressed by the freshness of the ingredients. Each dish was
+                  bursting with flavor, and the portion sizes were perfect. The
+                  service was quick and efficient.
+                </p>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={profile}
+                    alt="Description of image"
+                    width={60}
+                    height={200}
+                    className="pb-2"
+                  />
+
+                  <div className="">
+                    <h5 className="text-[#AC1518] font-semibold">
+                      Nafiz Salim
+                    </h5>
+                    <h5>Graphics Designer</h5>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="space-y-4">
+                <p>
+                I recently dined at your restaurant and wanted to share my
+                  experience. The food was absolutely delicious, and I was
+                  impressed by the freshness of the ingredients. Each dish was
+                  bursting with flavor, and the portion sizes were perfect. The
+                  service was quick and efficient, and the staff was incredibly
+                  friendly and welcoming.
+                </p>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={profile}
+                    alt="Description of image"
+                    width={60}
+                    height={200}
+                    className="pb-2"
+                  />
+
+                  <div className="">
+                    <h5 className="text-[#AC1518] font-semibold">
+                      Tayyab Iqbal
+                    </h5>
+                    <h5>Developer</h5>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
         <div className="col-span-3">
           <Image
