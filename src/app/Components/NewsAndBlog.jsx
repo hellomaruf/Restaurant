1ee213dfbcf2e33a32da4d6164bae0d5c51e73ed
maruf-ahmed <marufahamed1110@gmail.com>
2024-09-21 11:07:28 +0600

    Added news and blog section

---
+++ src/app/Components/NewsAndBlog.jsx
@@ -0,0 +1,76 @@
+"use client";
+import Image from "next/image";
+import { MdOutlineDateRange } from "react-icons/md";
+import { GoCommentDiscussion } from "react-icons/go";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+
+function NewsAndBlog() {
+  const newsAndBlogData = [
+    {
+      title: "The Ultimate Guide to Making Homemade Pizza",
+      img: "https://i.postimg.cc/YCSqjqVd/blogImg.png",
+      date: "02 Jan 2022",
+      description:
+        "Master the art of making authentic Italian pasta at home with this step-by-step guide. From choosing the right ingredients to cooking it al dente, learn all the secrets.",
+      comments: 12,
+    },
+    {
+      title: "Mastering the Art of Perfect Pasta at Home",
+      img: "https://i.postimg.cc/YCSqjqVd/blogImg.png",
+      date: "02 Jan 2022",
+      description:
+        "Discover the secret to making the best homemade pizza with a crispy crust, rich tomato sauce, and a variety of toppings. Perfect for a weekend treat!",
+      comments: 25,
+    },
+    {
+      title: "10-Minute Smoothie Recipes for Busy Mornings",
+      img: "https://i.postimg.cc/YCSqjqVd/blogImg.png",
+      date: "02 Jan 2022",
+      description:
+        "Start your day with a boost of energy! These smoothie recipes are not only quick to make but packed with nutrients to keep you going all day.",
+      comments: 8,
+    },
+  ];
+
+  return (
+      <div className='max-w-5xl mx-auto'>
+          <div className="max-w-2xl mx-auto mt-28 space-y-2">
+              <h1 className='text-4xl font-semibold text-center'>Latest news & Blog</h1>
+              <p className='text-center text-gray-600'>Discover easy recipes, cooking tips, and tasty trends to elevate your kitchen game. Join us on a delicious journey, one dish at a time!</p>
+          </div>
+      <div className="grid grid-cols-3 gap-4 mt-10">
+        {newsAndBlogData?.map((item, index) => (
+          <div className="shadow-lg" key={index}>
+            <div className="p-4">
+              <Image
+                src={item?.img}
+                alt="Description of image"
+                width={500}
+                height={200}
+                className="pb-4"
+              />
+              <div className='flex items-center gap-4'>
+                <div className='flex items-center gap-2 text-gray-500 text-sm'>
+                <MdOutlineDateRange className='text-xl text-[#A52A2A]' /> {item?.date}
+                </div>
+                <div className='flex items-center gap-2 text-gray-500 text-sm'>
+                <GoCommentDiscussion  className='text-xl  text-[#A52A2A]' />Comments ({item?.comments})
+                </div>
+                
+              </div>
+
+              <div className='mt-3 space-y-3'>
+                <h4 className='text-md font-semibold'>{item?.title}</h4>
+                <p className='text-gray-500'>{item?.description.slice(0,90)}</p>
+                <button className='flex items-center text-sm'>Read More <IoIosArrowRoundForward className='text-2xl' /></button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default NewsAndBlog;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import About from "./Components/About";
 import BestSellerDishes from "./Components/BestSellerDishes";
 import Feedback from "./Components/Feedback";
 import Hero from "./Components/Hero";
+import NewsAndBlog from "./Components/NewsAndBlog";
 import Team from "./Components/Team";
 import WhyChooseUs from "./Components/WhyChooseUs";
 import Work from "./Components/Work";
@@ -17,7 +18,8 @@ export default function Home() {
         <BestSellerDishes/>
         <Feedback/>
         <Team/>
-        <Work/>
+        <Work />
+        <NewsAndBlog/>
       </main>
     </div>
   );
